9ace314b1d1704601a43417a374755f83a080b4e
Peachuvu <pfefferkornstefanie@gmail.com>
2023-11-05 19:58:24 +0100

    komische Zeile, die wohl gebraucht wird, ist wieder drin

--- src/InOneWeekend/camera.h
+++ src/InOneWeekend/camera.h
@@ -92,6 +92,7 @@ class camera {
             wait(NULL);
         }
 
+        std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
         for (int j = 0; j < image_height; ++j)
         {
             for (int i = 0; i < image_width; ++i)
