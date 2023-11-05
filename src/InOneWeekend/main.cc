cf73857f08c795d639fe34ebabb4797495ff6c4e
Peachuvu <pfefferkornstefanie@gmail.com>
2023-11-05 20:38:28 +0100

    measure time auskommentiert

--- src/InOneWeekend/main.cc
+++ src/InOneWeekend/main.cc
@@ -78,10 +78,9 @@ int main() {
     cam.focus_dist    = 10.0;
 
     //start measuring time 
-    std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
-
+    //std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
     cam.render(world);
 
-    std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
-    std::cout << "Time difference = " << std::chrono::duration_cast<std::chrono::nanoseconds> (end - begin).count() << "[ns]" << std::endl;
+    //std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
+    //std::cout << "Time difference = " << std::chrono::duration_cast<std::chrono::nanoseconds> (end - begin).count() << "[ns]" << std::endl;
 }
